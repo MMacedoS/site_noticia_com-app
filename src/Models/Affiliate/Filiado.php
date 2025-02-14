7d68de1209fd087d0a86fa59ea876ec7e37746bd
Maria <marialuizaprado13@gmail.com>
2025-02-13 21:04:36 -0300

    created affiliate models

--- src/Models/Affiliate/Filiado.php
+++ src/Models/Affiliate/Filiado.php
@@ -8,5 +8,35 @@ class Filiado{
     
     use UuidTrait;
 
-    
+    public $id;
+    public string $uuid;
+    public int $pessoa_fisica_id;
+    public string $profissao;
+    public string $ativo;
+    public $created_at;
+    public $updated_at;
+
+    public function __construct () {}
+
+    public function create(
+        array $data
+    ): Filiado {
+        $filiado = new Filiado();
+        $filiado->id = $data['id'] ?? null;
+        $filiado->uuid = $data['uuid'] ?? this->generateUUID();
+        $filiado->pessoa_fisica_id = (int)$data['person_id'];
+        $filiado->profissao = $data['profession'] ?? null;
+        $filiado->ativo = (int)$data['active'] ?? 1;
+        $filiado->created_at = $data['created_at'] ?? null;
+        $filiado->updated_at = $data['updated_at'] ?? null;
+        return $filiado;    
+    }
+
+    public function update(array $data, Filiado $filiado): Filiado
+    {
+        $filiado->pessoa_fisica_id = $data['person_id'] ?? $filiado->pessoa_fisica_id;
+        $filiado->ativo = $data['active'] ?? $filiado->ativo;
+
+        return $filiado;
+    }
 }
