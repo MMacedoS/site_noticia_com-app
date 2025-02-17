79346e562c252f9e29748c57ec2c7fbd86ff8996
andreeezinho <andreeesapucaia@gmail.com>
2025-02-17 10:47:10 -0300

    feat: create methods for colaborator creating

--- src/Repositories/Colaborator/ColaboratorRepository.php
+++ src/Repositories/Colaborator/ColaboratorRepository.php
@@ -66,4 +66,96 @@ class ColaboratorRepository implements IColaboratorRepository {
         return $stmt->fetchAll(\PDO::FETCH_CLASS, self::CLASS_NAME);
     }
 
+    public function saveAll(array $data){
+        if(empty($data)){
+            return null;
+        }
+
+        try{
+            $userData = array_merge($data, [
+                'password' => 'sindsmut123'
+            ]);
+
+            $user = $this->usuarioRepository->create($userData);
+
+            $personData = array_merge($data, ['usuario_id' => $user->id]);
+            $person = $this->pessoaFisicaRepository->create($personData);
+
+            $colaboratorData = array_merge($data, ['pessoa_fisica_id' => $person->id]);
+            $colaborator= $this->create($colaboratorData);
+
+            return $colaborator;
+
+        }catch(\Throwable $th){
+            LoggerHelper::logInfo($th->getMessage());
+            return null;
+        }finally{
+            Database::getInstance()->closeConnection();
+        }
+    }
+
+    public function create(array $params){
+        $findColaborator = $this->findByColaboratorId($params);
+        if($findColaborator){
+            return $findColaborator;
+        }
+
+        $colaborator = $this->model->create($params);
+
+        try{
+            $sql = "INSERT INTO " . self::TABLE . "
+                set
+                    uuid = :uuid,
+                    setor_id = :sector_id,
+                    pessoa_fisica_id = :person_id
+            ";
+
+            $stmt = $this->conn->prepare($sql);
+
+            $stmt->execute([
+                ':uuid' => $colaborator->uuid,
+                ':sector_id' => $colaborator->sector_id,
+                ':person_id' => $colaborator->person_id
+            ]);
+
+        }catch(\Throwable $th){
+            LoggerHelper::logInfo($th->getMessage());
+            return null;
+        }finally{
+            Database::getInstance()->closeConnection();
+        }
+    }
+
+    public function findByColaboratorId(array $data) : ?Colaborador{
+        try{
+
+            $conditions = [];
+            $params = [];
+
+            if(!empty($data['person_id'])){
+                $conditions[] = "pessoa_fisica_id = :pessoa_fisica_id";
+                $params[':pessoa_fisica_id'] = $data['person_id'];
+            }
+
+            if(empty($conditions)){
+                return null;
+            }
+
+            $sql = "SELECT FROM " . self::TABLE . " WHERE " . implode(" AND ", $conditions);
+            
+            $stmt = $this->conn->prepare($sql);
+            $stmt->execute($params);
+
+            $stmt->setFetchMode(\PDO::FETCH_CLASS | \PDO::FETCH_PROPS_LATE, self::CLASS_NAME);
+            $result = $stmt->fetch();  
+
+            return $result ?: null;
+
+        }catch(\Throwable $th){
+            LoggerHelper::logInfo($th->getMessage());
+            return null;
+        }finally{
+            Database::getInstance()->closeConnection();
+        }
+    }
 }
