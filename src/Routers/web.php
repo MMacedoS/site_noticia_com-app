75461055690adae70bb2d588a7648ee97956c3e8
Maria <marialuizaprado13@gmail.com>
2025-02-18 17:39:39 -0300

    feat: create affiliate routes

--- src/Routers/web.php
+++ src/Routers/web.php
@@ -7,6 +7,7 @@ use App\Config\Router;
 use App\Controllers\v1\NotFound\NotFoundController;
 use App\Controllers\v1\Sector\SetorController;
 use App\Controllers\v1\Site\SiteController;
+use App\Controllers\v1\Affiliate\FiliadoRepository;
 
 $container = new Container();
 $appServiceProvider = new AppServiceProvider($container);
@@ -14,12 +15,14 @@ $appServiceProvider->registerDependencies();
 
 $siteController = $container->get(SiteController::class);
 $setorController = $container->get(SetorController::class);
+$filiadoController = $container->get(FiliadoController::class);
 
 $router = new Router();
 $auth = new Auth();
 
 $setorController = $container->get(SetorController::class);
 $siteController = $container->get(SiteController::class);
+$filiadoController = $container->get(FiliadoController::class);
 
 $notFoundController = new NotFoundController();
 
@@ -31,5 +34,13 @@ $router->create("GET", "/noticias", [$siteController, "noticias"], null);
 $router->create("GET", "/noticias/{title}", [$siteController, "noticia"], null);
 
 $router->create('GET', '/not-found', [$notFoundController, 'index']);
+
+//filiados
+$router->create('GET', '/filiados', [$filiadoController, 'index'], $auth);
+$router->create('GET', '/filiados/criar', [$filiadoController, 'create'], $auth);
+$router->create('POST', '/filiados/criar', [$filiadoController, 'store'], $auth);
+$router->create('GET', '/filiados/{id}/editar', [$filiadoController, 'edit'], $auth);
+$router->create('POST', '/filiados/{id}/editar', [$filiadoController, 'update'], $auth);
+$router->create('DELETE', '/filiados/{id}', [$filiadoController, 'destroy'], $auth);
 
 return $router;
