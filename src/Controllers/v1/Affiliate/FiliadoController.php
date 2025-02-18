7e619c3c583268bc0fce1a83bcda3f0a3a3b507e
Maria <marialuizaprado13@gmail.com>
2025-02-18 16:41:46 -0300

    feat: create index, create and store methods in controller

--- src/Controllers/v1/Affiliate/FiliadoController.php
+++ src/Controllers/v1/Affiliate/FiliadoController.php
@@ -20,8 +20,63 @@ class FiliadoController extends Controller{
         parent::__construct();
         $this->filiadoRepository = $filiadoRepository;
         $this->pessoaFisicaRepository = $pessoaFisicaRepository;
-
-
     }
 
+    public function index(Request $request){
+        $params = $request->getQueryParams();
+
+        $filiados = $this->filiadoRepository->allAffiliates($params);
+        $perpage = 10;
+        $currentPage = $request->getParams('page') ? (int)$request->getParams('page') : 1;
+        $paginator = new Paginator($filiados, $perPage, $currentPage);
+        $paginatedBoards = $paginator->getPaginatedItems();
+
+        return $this->router->view('affiliate/index', [
+            'filiados' => $paginatedBoards,
+            'links' => $paginator->links(),
+            'name_email' => $params['name_email'] ?? null,
+            'situation' => $params['situation'] ?? null
+        ]);
+    }
+
+    public function create(Request $request){
+        return $this->router->view('affiliate/create', [
+            'active' => 'cadastro'
+        ]);
+    }
+
+    public function store(Request $request){
+        $data = $request->getBodyParams();
+
+        $validator = new validator($data);
+
+        $rules = [
+            'name' => 'required|min:1|max:100',
+            'email' => 'required|email',
+            'doc' => 'required'
+        ];
+
+        if(!$validator->validate($rules)){
+            return $this->router->view('affiliate/create', [
+                'errors' => $validator->getErrors()
+            ]);
+        }
+
+        try{
+            $create = $this->filiadoRepository->saveAll($data);
+
+            return $this->router->redirect('filiados');
+        }catch(\Exception $e){
+            return $this->router->view('affiliate/create', [ 
+                'active' => 'cadastro',
+                'error' => 'Erro ao criar o colaborador: ' . $e->getMessage()
+            ]);
+        }
+    }
+
+    public function edit(Request $request, $id){}
+
+    public function update(Request $request, $id){}
+
+    public function destroy(Request $request, $id){}
 }
