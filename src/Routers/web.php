19db3f3717385f90e8fd038a8c678f5b42cc7684
andreeezinho <andreeesapucaia@gmail.com>
2025-02-20 10:07:37 -0300

    feat: create routers and views archives

--- src/Routers/web.php
+++ src/Routers/web.php
@@ -8,6 +8,7 @@ use App\Controllers\v1\Dashboard\DashboardController;
 use App\Controllers\v1\NotFound\NotFoundController;
 use App\Controllers\v1\Sector\SetorController;
 use App\Controllers\v1\Site\SiteController;
+use App\Controllers\v1\New\NoticiaController;
 
 $container = new Container();
 $appServiceProvider = new AppServiceProvider($container);
@@ -16,11 +17,12 @@ $appServiceProvider->registerDependencies();
 $siteController = $container->get(SiteController::class);
 $dashboardController = $container->get(DashboardController::class);
 $setorController = $container->get(SetorController::class);
+$noticiaController = $container->get(NoticiaController::class);
 
 $router = new Router();
 $auth = new Auth();
 
-$setorController = $container->get(SetorController::class);
+$noticiaController = $container->get(NoticiaController::class);
 $siteController = $container->get(SiteController::class);
 
 $notFoundController = new NotFoundController();
@@ -35,5 +37,13 @@ $router->create("GET", "/noticias/{title}", [$siteController, "noticia"], null);
 $router->create('GET', '/not-found', [$notFoundController, 'index']);
 
 $router->create('GET', "/dashboard", [$dashboardController, 'index'], null);
+
+//noticias
+$router->create('GET', '/noticias', [$noticiaController, 'index'], $auth);
+$router->create('GET', '/noticias/criar', [$noticiaController, 'create'], $auth);
+$router->create('POST', '/noticias/criar', [$noticiaController, 'store'], $auth);
+$router->create('GET', '/noticias/{id}/editar', [$noticiaController, 'edit'], $auth);
+$router->create('POST', '/noticias/{id}/editar', [$noticiaController, 'update'], $auth);
+$router->create('DELETE', '/noticias/{id}', [$noticiaController, 'destroy'], $auth);
 
 return $router;
